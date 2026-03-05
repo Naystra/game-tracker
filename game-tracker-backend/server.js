ebb0f7e5938409b8df75bbff07d7bbe94a7df536
Naystra <ton-email-associ--Naystra@github.com>
2026-03-05 16:38:46 +0100

    t

--- game-tracker-backend/server.js
+++ game-tracker-backend/server.js
@@ -35,17 +35,17 @@ app.use('/uploads', express.static('uploads'));
 
 
 // Connexion MongoDB  
-mongoose.connect(process.env.MONGO_URI, {
+
+
+
+// Lancement du serveur
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => { mongoose.connect(process.env.MONGO_URI, {
     serverSelectionTimeoutMS: 30000,
     socketTimeoutMS: 45000,
 })
 .then(() => console.log('MongoDB connecté'))
 .catch(err => console.error('Erreur MongoDB :', err));
-
-
-// Lancement du serveur
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
     console.log(`Serveur lancé sur : http://localhost:${PORT}`);
 });
 
