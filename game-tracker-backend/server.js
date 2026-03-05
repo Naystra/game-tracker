c087fb92144f3cc821326c29b28c48bd2bd2baff
Naystra <ton-email-associ--Naystra@github.com>
2026-03-05 14:10:44 +0100

    test

--- game-tracker-backend/server.js
+++ game-tracker-backend/server.js
@@ -16,6 +16,9 @@ app.use(cors());
 app.use(express.json());
 
 
+console.log("MONGO_URI utilisée:", process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+
 // Connexion MongoDB :
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connecté'))
